d8bc55adff0fb0eacfff484612dc8095346b2daf
Colin Roberson <roberson.colin@gmail.com>
2024-10-04 15:45:07 -0500

    Delete userdata.sh

--- userdata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dnf update -y
-dnf install -y httpd
-systemctl start httpd
-systemctl enable httpd
-TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60"`
-INSTANCE=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id`
-echo "Hello, World! From: $INSTANCE" > /var/www/html/index.html
