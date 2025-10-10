9e0ba22406054cc2af71a1e865e330e8c1b0eabc
Colin Roberson <roberson.colin@gmail.com>
2025-10-10 16:14:58 -0500

    attempting to remove stage

--- stage/data-stores/mysql/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "address" {
-  value = aws_db_instance.example.address
-  description = "Connect to the DB at this endpoint"
-}
-
-output "port" {
-  value = aws_db_instance.example.port
-  description = "The port the DB is listening on"
-}
